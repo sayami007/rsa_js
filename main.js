5348208d924894a1cfcaf097be101c3359bdb2f2
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2018-11-28 06:24:18 +0545

    Getting value of p and q from user

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-var p = 11;
-var q = 3;
-var e = 3;
-var d = 7;
-
-var phi = (p - 1) * (q - 1);
-var n = p * q;
-
 function getRandomNumber() {
     return Math.floor(Math.random() * 1000) + 1;
 }
@@ -19,18 +11,16 @@ function gcd(p, q) {
 }
 
 
-function getPower(a, b) {
-
-
-}
-
-
 function getEncryption() {
+    var p = document.querySelector("#p").value;
+    var q = document.querySelector("#q").value;
+    var phi = (p - 1) * (q - 1);
+    var n = p * q;
+    var e = 3;
+    var d = 7;
     var Message = document.querySelector("#message").value;
     var encryptMessage = Math.pow(Message, e) % n;
     var decryptMessage = Math.pow(encryptMessage, d) % n;
     document.querySelector("#encrypt").innerHTML = encryptMessage;
     document.querySelector("#decrypt").innerHTML = decryptMessage;
-
-
 }
